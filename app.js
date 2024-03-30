1d996bc876d7b4a39423e7b8804f0fba16c91ee2
Nikita <dubblerner.2@gmail.com>
2024-03-31 01:18:21 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const arrow = document.querySelector('.arrow-1').addEventListener('click', goTo);
+
+function goTo() {
+    window.location.href = 'https://www.youtube.com/watch?v=GmPZUCd89p8';
+    console.log('click')
+}
